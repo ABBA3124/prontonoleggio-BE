efbe425458eb69d137f2b54bc67c5f552812c327
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-13 18:58:18 +0200

    Initial commit

---
+++ src/main/java/davideabbadessa/prontonoleggio_BE/ProntonoleggioBeApplication.java
@@ -0,0 +1,13 @@
+package davideabbadessa.prontonoleggio_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProntonoleggioBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProntonoleggioBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davideabbadessa/prontonoleggio_BE/ProntonoleggioBeApplicationTests.java
@@ -0,0 +1,13 @@
+package davideabbadessa.prontonoleggio_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProntonoleggioBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
