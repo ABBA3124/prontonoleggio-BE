018fc7e8d17f5155af38e594a05a717d2ab92c38
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-15 16:43:00 +0200

    Update UtenteController.java
    Add Mapping

--- src/main/java/davideabbadessa/prontonoleggio_BE/crontrollers/UtenteController.java
+++ src/main/java/davideabbadessa/prontonoleggio_BE/crontrollers/UtenteController.java
@@ -1,13 +1,14 @@
 package davideabbadessa.prontonoleggio_BE.crontrollers;
 
-import davideabbadessa.prontonoleggio_BE.entities.Utente;
+import davideabbadessa.prontonoleggio_BE.entities.utente.Utente;
+import davideabbadessa.prontonoleggio_BE.payloads.utente.NuovoUtenteDTO;
 import davideabbadessa.prontonoleggio_BE.services.UtenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -19,10 +20,24 @@ public class UtenteController {
 
 
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_SUPERADMIN')")
     public Page<Utente> getAllUtenti(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "id") String sortBy) {
         return this.utenteService.getAllUtenti(page, size, sortBy);
     }
 
+    // /me visualizza il mio profilo
+    @GetMapping("/me")
+    public Utente getProfilo(@AuthenticationPrincipal Utente currentAuthenticatedUtente) {
+        return currentAuthenticatedUtente;
+
+    }
+
+
+    //update informazioni del profilo
+    @PutMapping("/me")
+    public Utente updateProfilo(@AuthenticationPrincipal Utente currentAuthenticatedUtente, @Validated @RequestBody NuovoUtenteDTO body) {
+        return this.utenteService.updateProfilo(currentAuthenticatedUtente.getId(), body);
+    }
 
 }
 
