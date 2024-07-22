5685d897cfb78eea3763038ee0bb775b834d0958
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-22 21:36:41 +0200

    Update UtenteRepository.java

--- src/main/java/davideabbadessa/prontonoleggio_BE/repositories/UtenteRepository.java
+++ src/main/java/davideabbadessa/prontonoleggio_BE/repositories/UtenteRepository.java
@@ -2,13 +2,14 @@ package davideabbadessa.prontonoleggio_BE.repositories;
 
 import davideabbadessa.prontonoleggio_BE.entities.utente.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UtenteRepository extends JpaRepository<Utente, UUID> {
+public interface UtenteRepository extends JpaRepository<Utente, UUID>, JpaSpecificationExecutor<Utente> {
 
     Optional<Utente> findByEmail(String email);
 
