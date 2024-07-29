eaccf5ce3374b64961fa4ae95262148858fca227
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-30 00:10:26 +0200

    Update PrenotazioneRepository.java

--- src/main/java/davideabbadessa/prontonoleggio_BE/repositories/Prenotazione/PrenotazioneRepository.java
+++ src/main/java/davideabbadessa/prontonoleggio_BE/repositories/Prenotazione/PrenotazioneRepository.java
@@ -4,6 +4,7 @@ import davideabbadessa.prontonoleggio_BE.entities.prenotazioni.Prenotazione;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface PrenotazioneRepository extends JpaRepository<Prenotazione, UUID> {
+public interface PrenotazioneRepository extends JpaRepository<Prenotazione, UUID>, JpaSpecificationExecutor<Prenotazione> {
     @Query("SELECT p FROM Prenotazione p WHERE p.veicolo.id = :veicoloId AND p.dataFine >= :dataInizio AND p.dataInizio <= :dataFine")
     List<Prenotazione> findPrenotazioniSovrapposte(UUID veicoloId, LocalDate dataInizio, LocalDate dataFine);
 
