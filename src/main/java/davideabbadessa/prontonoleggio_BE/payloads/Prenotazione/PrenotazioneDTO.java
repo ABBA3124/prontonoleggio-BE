2b0267d8a38891ce894c2734589fb688e1e791db
ABBA3124 <davideabbadessa73@gmail.com>
2024-08-02 03:15:39 +0200

    Update PrenotazioneDTO.java

--- src/main/java/davideabbadessa/prontonoleggio_BE/payloads/Prenotazione/PrenotazioneDTO.java
+++ src/main/java/davideabbadessa/prontonoleggio_BE/payloads/Prenotazione/PrenotazioneDTO.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public record PrenotazioneDTO(
-        UUID id,
         UUID veicoloId,
         UUID utenteId,
         LocalDate dataInizio,
